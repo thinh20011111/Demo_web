dfed84252659c9f013145bf715835998731206ba
Lê Minh Thịnh <645859@sv.vnua.edu.vn>
2022-07-09 19:24:30 +0700

    Web demo starbucks

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function imgSlider(anything){
+    document.querySelector('.starBucks').src = anything;
+}
+
+function changeCirleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+
+}
